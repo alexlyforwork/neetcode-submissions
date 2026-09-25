2f79a7069dfa30a6c637bf6a7c62195563c6da07
Alex Ly <alexlyforwork@gmail.com>
2026-09-25 10:30:23 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2)<len(s1):
+            return False
+        freq1,freq2 = {},{}
+        length = len(s1)
+        n2 = len(s2)
+        for i in range(len(s1)):
+            freq1[s1[i]] = freq1.get(s1[i],0)+1
+            freq2[s2[i]] = freq2.get(s2[i],0)+1
+        
+        for i in range(n2-length+1):
+            if freq1 == freq2:
+                return True
+
+            letter = s2[i]
+            freq2[letter]-=1
+            if freq2[letter]==0:
+                del freq2[letter]
+            if i+length==n2:
+                break
+            freq2[s2[i+length]]=freq2.get(s2[i+length],0)+1
+        return False
+
